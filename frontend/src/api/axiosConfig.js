9be39d285d5de6c2cdd1496e8846afbe23b69541
DharunMR <maddharun56gmail.com>
2025-12-19 02:35:28 +0530

    changed backend url

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
 export default axios.create({
-    baseURL:'http://localhost:8080',
+    baseURL:"http://backendapp-service:8080",
     headers:{'Content-Type':'application/json'},
     withCredentials: true,
 })
